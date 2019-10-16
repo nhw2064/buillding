8768250f8345ede76bd9286e5d85346d04e7bfba
nhw2064 <51572581+nhw2064@users.noreply.github.com>
2019-10-16 18:08:31 +0900

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,4 +1,9 @@
 $(document).ready(function () {
+	//문서 로딩시 첫번째 본문에 클래스명 추가하여 텍스트 애니메이션 시작됨
+	setTimeout(function () {
+		$('#content1').addClass('on');
+	}, 300);
+
     $(window).on('scroll', function () {
         var scrollT = $(this).scrollTop();
         console.log(scrollT)
@@ -18,3 +23,4 @@ $(document).ready(function () {
         });
     });
 });
+
